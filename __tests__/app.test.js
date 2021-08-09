db64313b07f96333bd26a02f10a68dc1b7b5efcd
Jon Fairbanks <fairplay89@gmail.com>
2021-08-09 16:44:45 -0700

    Prettier

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,13 @@
-const request = require("supertest");
-const app = require("../app");
+const request = require('supertest')
+const app = require('../app')
 
-describe("Verify the site loads", () => {
-  test("Response should equal HTTP 200", done => {
+describe('Verify the site loads', () => {
+  test('Response should equal HTTP 200', (done) => {
     request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
+      .get('/')
+      .then((response) => {
+        expect(response.statusCode).toBe(200)
+        done()
+      })
+  })
+})
