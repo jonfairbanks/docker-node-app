bde64fe3ad91fb1e3e823a07c2879c3bffeae7e6
Jon Fairbanks <fairplay89@gmail.com>
2022-03-27 14:55:23 -0700

    Lint Fixes

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-const express = require('express')
-const app = express()
-const os = require('os')
-const dayjs = require('dayjs')
-const advancedFormat = require('dayjs/plugin/advancedFormat')
+const express = require('express');
 
-app.set('view engine', 'ejs')
-dayjs.extend(advancedFormat)
+const app = express();
+const os = require('os');
+const dayjs = require('dayjs');
+const advancedFormat = require('dayjs/plugin/advancedFormat');
+
+app.set('view engine', 'ejs');
+dayjs.extend(advancedFormat);
 
 // Sample Middleware
 app.use((_req, res, next) => {
-  res.removeHeader('X-Powered-By')
-  res.setHeader('X-Hostname', os.hostname())
-  res.setHeader('X-Timestamp', Date.now())
-  res.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate')
-  res.setHeader('Expires', '-1')
-  res.setHeader('Pragma', 'no-cache')
-  next()
-})
+  res.removeHeader('X-Powered-By');
+  res.setHeader('X-Hostname', os.hostname());
+  res.setHeader('X-Timestamp', Date.now());
+  res.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.setHeader('Expires', '-1');
+  res.setHeader('Pragma', 'no-cache');
+  next();
+});
 
 // Create a public directory for Images/CSS/etc.
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(`${__dirname}/public`));
 
 // Handle Incoming Requests
 app.get('/', (req, res) => {
-  const ip =
-    req.headers['x-original-forwarded-for'] ||
-    req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    (req.connection.socket ? req.connection.socket.remoteAddress : 'Unknown')
+  const ip = req.headers['x-original-forwarded-for']
+    || req.headers['x-forwarded-for']
+    || req.connection.remoteAddress
+    || req.socket.remoteAddress
+    || (req.connection.socket ? req.connection.socket.remoteAddress : 'Unknown');
 
   res.render('index', {
-    ip: ip,
+    ip,
     hostname: os.hostname(),
     user_agent: req.headers['user-agent'],
     timestamp: dayjs().format('MMM Do YYYY, h:mm:ss A'),
-  })
-})
+  });
+});
 
 // Handle livenessProbe
 app.get('/healthz', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   res
     .send({
       response: {
@@ -49,7 +49,7 @@ app.get('/healthz', (req, res) => {
         clientSourceIP: ip,
       },
     })
-    .status(200)
-})
+    .status(200);
+});
 
-module.exports = app
+module.exports = app;
